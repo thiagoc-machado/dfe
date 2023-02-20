eba00630f247e31d56be2501c6713eb0f48e9599
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-02-20 12:09:55 +0100

    ads

--- ads/models.py
+++ ads/models.py
@@ -1,35 +1,55 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
-from django.contrib.auth.models import User
 from django.conf import settings
 
 from taggit.managers import TaggableManager
 
-class Ad(models.Model) :
+
+class Ad(models.Model):
+
     title = models.CharField(
-            max_length=200,
-            validators=[MinLengthValidator(2, "Title must be greater than 2 characters")]
+        max_length=200,
+        validators=[
+            MinLengthValidator(2, "Title must be greater than 2 characters"),
+        ]
     )
+    
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    
     text = models.TextField()
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    # Picture
     picture = models.BinaryField(null=True, editable=True)
     content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
+    
+    # Comments
     comments = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Comment', related_name='comments_owned')
+    
+    # Favorites
     favorites = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Fav', related_name='favorite_ads')
+    
+    # Tags
+    tags = TaggableManager(blank=True)
+    
+    # hidden
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-       # Tags
-    tags = TaggableManager(blank=True)
-
+    class Meta:
+        ordering = ['-updated_at']
+    
     # Shows up in the admin list
     def __str__(self):
         return self.title
 
-class Comment(models.Model) :
+
+class Comment(models.Model):
+    
     text = models.TextField(
-        validators=[MinLengthValidator(3, "Comment must be greater than 3 characters")]
+        validators=[
+            MinLengthValidator(3, "Comment must be greater than 3 characters"),
+        ]
     )
 
     ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
@@ -43,11 +63,11 @@ class Comment(models.Model) :
         if len(self.text) < 15 : return self.text
         return self.text[:11] + ' ...'
 
-class Fav(models.Model) :
+class Fav(models.Model):
     ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    # https://docs.djangoproject.com/en/3.0/ref/models/options/#unique-together
+    # https://docs.djangoproject.com/en/4.0/ref/models/options/#unique-together
     class Meta:
         unique_together = ('ad', 'user')
 
