a35bdf6421558ad5af50ef17aa025d1af4e82a18
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-02-20 12:30:53 +0100

    ads

--- ads/forms.py
+++ ads/forms.py
@@ -1,9 +1,7 @@
-from ads.models import Ad
-from ads.humanize import naturalsize
-
 from django import forms
+from ads.models import Ad
 from django.core.files.uploadedfile import InMemoryUploadedFile
-
+from ads.humanize import naturalsize
 
 # Create the form class.
 class CreateForm(forms.ModelForm):
@@ -13,52 +11,40 @@ class CreateForm(forms.ModelForm):
     # Call this 'picture' so it gets copied from the form to the in-memory model
     # It will not be the "bytes", it will be the "InMemoryUploadedFile"
     # because we need to pull out things like content_type
-    picture = forms.FileField(
-        required=False,
-        label='File to Upload <= ' + max_upload_limit_text
-    )
+    picture = forms.FileField(required=False, label='File to Upload <= '+max_upload_limit_text)
     upload_field_name = 'picture'
 
     # Hint: this will need to be changed for use in the ads application :)
     class Meta:
         model = Ad
-        fields = ['title', 'price', 'text', 'picture', 'tags']  # Picture is manual
+        fields = ['title', 'text', 'price', 'picture', 'tags']  # Picture is manual
 
     # Validate the size of the picture
-    def clean(self):
+    def clean(self) :
         cleaned_data = super().clean()
-        pic = cleaned_data.get('picture')
-        
-        if pic is None:
-            return
-        
-        if len(pic) > self.max_upload_limit:
-            self.add_error(
-                'picture',
-                "File must be < " + self.max_upload_limit_text + " bytes"
-            )
-            
+        ad = cleaned_data.get('picture')
+        if ad is None : return
+        if len(ad) > self.max_upload_limit:
+            self.add_error('picture', "File must be < "+self.max_upload_limit_text+" bytes")
+
     # Convert uploaded File object to a picture
-    def save(self, commit=True):
+    def save(self, commit=True) :
         instance = super(CreateForm, self).save(commit=False)
 
         # We only need to adjust picture if it is a freshly uploaded file
         f = instance.picture   # Make a copy
-        
         if isinstance(f, InMemoryUploadedFile):  # Extract data from the form to the model
-            bytearr = f.read()
+            bytearr = f.read();
             instance.content_type = f.content_type
             instance.picture = bytearr  # Overwrite with the actual image data
 
         if commit:
             instance.save()
-            
             # save tags
             self.save_m2m()
-        
+
         return instance
 
-# strip means to remove whitespace from the beginning and the end before storing the column
 class CommentForm(forms.Form):
     comment = forms.CharField(required=True, max_length=500, min_length=3, strip=True)
 
@@ -66,3 +52,4 @@ class CommentForm(forms.Form):
 # https://stackoverflow.com/questions/2472422/django-file-upload-size-limit
 # https://stackoverflow.com/questions/32007311/how-to-change-data-in-django-modelform
 # https://docs.djangoproject.com/en/3.0/ref/forms/validation/#cleaning-and-validating-fields-that-depend-on-each-other
+
